3fc87a910b2e53c3f6fb20064928f908fce2dbe2
aldocosta <aldocostasantos@hotmail.com>
2016-12-20 19:22:04 -0200

    Correções de calculo da ficha nova, implementação de pesquisa de vantagens, pericias e desvantagens em uma ficha criada

--- gmobi - build 1.bat
+++ gmobi - build 1.bat
@@ -1,5 +1,8 @@
 @echo off
 
+cd/
+cd users\aldo\documents\github\gmobile
+
 set ANDROID_HOME=C:\Program Files (x86)\Android\android-sdk
 set PATH=%PATH%;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools
 
--- gmobi - build 2.bat
+++ gmobi - build 2.bat
@@ -6,13 +6,13 @@ cd\
 cd program files (x86)\java\jdk1.7.0_55\bin
 
 
-jarsigner -verbose -keystore d:\\gmobi.keystore  C:/Users/Aldo/Documents/GitHub/Gmobi/platforms/android/build/outputs/apk/android-release-unsigned.apk AcsMobileApps
+jarsigner -verbose -keystore d:\\gmobi.keystore  C:/Users/Aldo/Documents/GitHub/Gmobile/platforms/android/build/outputs/apk/android-release-unsigned.apk AcsMobileApps
 
 
 cd..\..\..
 cd android\android-sdk\build-tools\24.0.0
 
-zipalign -f -v 4 C:/Users/Aldo/Documents/GitHub/Gmobi/platforms/android/build/outputs/apk/android-release-unsigned.apk C:/Users/Aldo/Documents/GitHub/Gmobi/platforms/android/build/outputs/apk/android-release-zipsigned.apk
+zipalign -f -v 4 C:/Users/Aldo/Documents/GitHub/Gmobile/platforms/android/build/outputs/apk/android-release-unsigned.apk C:/Users/Aldo/Documents/GitHub/Gmobile/platforms/android/build/outputs/apk/android-release-zipsigned.apk
 
 echo Finalizado!
 pause
